e6416d64a380fda7cc1ddee1c87037f44f69bc5f
Pratiksha-555 <76684620+Pratiksha-555@users.noreply.github.com>
2023-02-27 23:09:17 +0530

    Update sketch.py

--- sketch.py
+++ sketch.py
@@ -1,35 +1,50 @@
+#import opencv libraries
 import cv2
 import matplotlib.pyplot as plt
-path=r'C:\Users\pratiksha\Desktop\dog.jpg'
+
+#read image using opencv
+path=r'C:\Users\pawar shubham govind\Desktop\pratu.jpeg'
+
+
 img=cv2.imread(path)
-cv2.imshow('original image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-plt.imshow(img)
-plt.axis(False)
-plt.show()
-plt.imshow(img[:,:,::-1])
-plt.axis(False)
-plt.show()
-RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(RGB_img)
-plt.axis(False)
-plt.show()
+
+#show image using opencv
+def showimage():
+    #grey_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('original image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+#convert original image into  grey image using cvtColor of opdncv
 grey_img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 invert_img=cv2.bitwise_not(grey_img)
-#invert_img=255-grey_img
+
 blur_img=cv2.GaussianBlur(invert_img, (111,111),0)
+
 invblur_img=cv2.bitwise_not(blur_img)
-#invblur_img=255-blur_img
+
 sketch_img=cv2.divide(grey_img,invblur_img, scale=256.0)
+
 cv2.imwrite('sketch.png', sketch_img)
-cv2.imshow('sketch image',sketch_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def displaysketch():
+    cv2.imshow('sketch image',sketch_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+# Call main function
+if _name_ == '_main_':
+    showimage()
+    displaysketch()	
+
+
 plt.figure(figsize=(14,8))
 plt.subplot(1,2,1)
 plt.title('Original image', size=18)
-plt.imshow(RGB_img)
+plt.imshow(img)
 plt.axis('off')
 plt.subplot(1,2,2)
 plt.title('Sketch', size=18)
